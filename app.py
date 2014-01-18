376d1e8cd57f8a3cb50703506408fed4dd63e72a
Derek Eder <derek.eder@gmail.com>
2014-01-17 22:02:55 -0600

    iterating through a list of linkedin urls

--- app.py
+++ app.py
@@ -11,19 +11,25 @@ app = Flask(__name__)
 # ROUTES
 @app.route('/')
 def index():
-  # Define CONSUMER_KEY, CONSUMER_SECRET,  
-  # USER_TOKEN, and USER_SECRET from the credentials 
-  # provided in your LinkedIn application
-
-  # Instantiate the developer authentication class
+  # Instantiate the linkedin developer authentication class
   authentication = linkedin.LinkedInDeveloperAuthentication(settings.CONSUMER_KEY, settings.CONSUMER_SECRET, 
                                                             settings.USER_TOKEN, settings.USER_SECRET, 
                                                             settings.RETURN_URL, linkedin.PERMISSIONS.enums.values())
 
   # Pass it in to the app...
   application = linkedin.LinkedInApplication(authentication)
-  resp = make_response(json.dumps(application.get_profile()))
 
+  f = open('data/linkedin_profiles.json', 'rb')
+  people_urls = json.loads(f.read())
+  f.close()
+
+  profiles = []
+  selectors=['id', 'first-name', 'last-name', 'location', 'skills', 'educations']
+  for person_url in people_urls:
+    profiles.append(application.get_profile(member_url=person_url, 
+                                            selectors=selectors))
+  
+  resp = make_response(json.dumps(profiles))
   resp.headers['Content-Type'] = 'application/json'
   return resp
 
